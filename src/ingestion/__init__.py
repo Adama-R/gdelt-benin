967b91f5da940f7397ae7be322a5990b93746971
Adama-R <adamaadouayom@gmail.com>
2026-05-11 11:40:21 +0000

    Une mise à jour à été faite au niveau duc chemin de lecture des modules et constants.
    Une mise à jour a été faite au niveau du fichier permettant la lecture de la fonction de filtre des colonnes destiné à permettre la lecture des information du Bénin.

--- src/ingestion/__init__.py
+++ src/ingestion/__init__.py
@@ -1,11 +1,11 @@
 from .fetch import generate_gdelt_urls
 from .filter import link_mentions_to_events
-from processing.clean import filter_benin
+from ..processing import filter_benin
 from .download import download_file_stream, download_file_stream_on_local
 from .parse import parse_gdelt_zip, apply_gdelt_schema
 from .pipeline import fetch_and_parse, process_gdelt_pipeline_to_csv
 from .loader import load_gdelt_data
-from core.constants import (
+from ..core.constants import (
     GDELT_EVENT_COLUMNS,
     GDELT_GKG_COLUMNS,
     GDELT_MENTION_COLUMNS,
